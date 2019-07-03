cbdb2236cd44b51d26511251a9774ea88530a4e2
Muyun <muyun@nuaa.edu.cn>
2019-07-03 10:03:23 +0800

    Update Notes

--- OS-course/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-     // 开始列举进程信息
-     if (!Process32First(hSnapshot, &pe32))
-     {
-          printf("Process32First() 失败");
-          CloseHandle(hSnapshot); // 关闭句柄
-          return 0;
-     }
-     
